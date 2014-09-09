9935b0c3a67b394b1327d8c9a08dbd38e5b5178a
Taylor Baldwin <taylorbaldwin@gmail.com>
2014-09-09 11:45:39 -0400

    lower learning rate, but better learning

--- public/js/Q.js
+++ public/js/Q.js
@@ -8,7 +8,7 @@ var Q = (function() {
         'persist': false,
         'saveInterval': 5000,
         'discover': 0.0,
-        'alpha': 0.4,
+        'alpha': 0.2,
         'rewards': {
             'alive': 0,
             'win': 10,
